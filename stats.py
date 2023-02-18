72e51f5b2e1b6142ecbb2bb859be1cb54f7a8af6
philwoz <61164019+philwoz@users.noreply.github.com>
2023-02-18 11:49:47 +0000

    added fixtures.html and table to display fixtures

--- stats.py
+++ stats.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+@app.route('/fixtures')
+def fixtures_page():
+    games = [
+        {'id': 1, 'home': "Nott'm Forest", 'away': 'Man City' },
+        {'id': 2, 'home': "Man United", 'away': 'Leicester'},
+        {'id': 3, 'home': "Chelsea", 'away': 'Southampton'},
+    ]
+    return render_template('fixtures.html', games=games)
