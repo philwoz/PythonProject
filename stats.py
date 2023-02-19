265af81b515f153523fb0a03c911fefaaaad84b9
philwoz <61164019+philwoz@users.noreply.github.com>
2023-02-19 11:17:51 +0000

    added base/tables templates

--- stats.py
+++ stats.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stats.db'
+db = SQLAlchemy(app)
+
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    home = db.Column(db.String(), nullable=False)
+    away = db.Column(db.String(), nullable=False)
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -14,3 +26,14 @@ def fixtures_page():
         {'id': 3, 'home': "Chelsea", 'away': 'Southampton'},
     ]
     return render_template('fixtures.html', games=games)
+
+@app.route('/tables')
+def tables_page():
+    table = [
+        {'id': 1, 'team': 'Man City', 'played': 16, 'W': 10, 'D': 3, 'L': 3, 'GD': 24, 'PTS': 51},
+        {'id': 2, 'team': "Man United", 'played': 16, 'W': 9, 'D': 5, 'L': 3, 'GD': 18, 'PTS': 48},
+        {'id': 3, 'team': 'Leicester', 'played': 16, 'W': 8, 'D': 4, 'L': 3, 'GD': 4, 'PTS': 47},
+        {'id': 4, 'team': 'Southampton', 'played': 16, 'W': 7, 'D': 3, 'L': 3, 'GD': -4,'PTS': 30},
+        {'id': 5, 'team': 'Chelsea', 'played': 16, 'W': 8, 'D': 3, 'L': 2, 'GD': -24,'PTS': 25}
+    ]
+    return render_template('tables.html', table=table)
